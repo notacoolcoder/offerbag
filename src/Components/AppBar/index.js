af00dac10a944c859784439a80a81110ed115053
akshaycj <akshaycj999@gmail.com>
2018-10-10 00:28:25 +0530

    Logo-Text Updated

--- src/Components/AppBar/index.js
+++ src/Components/AppBar/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import "./index.css";
+import textLogo from "../../Res/textlogo.png";
 export default class index extends Component {
   render() {
     return (
       <div className="appbar-main">
-        <h3 style={{ color: "white" }}>OfferBag</h3>
+        <img src={textLogo} />
+        {/* <h3 style={{ color: "white" }}>OfferBag</h3> */}
       </div>
     );
   }
